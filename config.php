df9ef4973d005b3438a97414e29017193416aa65
lenochware <lenochware@gmail.com>
2016-01-16 13:33:07 +0100

    config

---
+++ config.php
@@ -0,0 +1,14 @@
+<?php
+
+$config = array(
+	'test.db' => 'pdo_mysql://root@localhost/test/utf8',
+
+	'pclib.compatibility' => array(
+		'tpl_syntax' => true,
+		'sql_syntax' => true,
+		'controller_underscore_postfixes' => true,
+	),
+ 
+);
+
+?>
--- index.php
+++ index.php
@@ -3,11 +3,12 @@
 require 'libs/pclib/pclib.php';
 
 $app = new App('demo-app');
+$app->addConfig('config.php');
 
-$app->db = new Db('pdo_mysql://root@localhost/test/utf8');
+$app->db = new Db($app->config['test.db']);
 $app->setLayout('tpl/website.tpl');
 
-$grid = new Grid();
+$grid = new Grid('tpl/people.tpl');
 $grid->setQuery('select * from PEOPLE');
 
 $app->layout->values['CONTENT'] = $grid->html();
